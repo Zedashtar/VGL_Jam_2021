cc9c4f0b5cd9617fcfa9849f2443c12b648b5c8e
Ben <benjamin.matheron@hotmail.fr>
2021-11-05 23:07:39 +0100

    Use Animation Curve for Dash
    upgraded game controller

--- VGL_2021/Assets/Scripts/GameController.cs
+++ VGL_2021/Assets/Scripts/GameController.cs
@@ -12,7 +12,7 @@ public class GameController : MonoBehaviour
 
 
     [Space][Header("Manager")]
-    public bool started;
+    public bool running;
     public GameObject level;
     public Player player;
     
@@ -21,14 +21,19 @@ public class GameController : MonoBehaviour
     public bool isJump => Input.GetButtonDown("Jump");
     public bool isDash => Input.GetButtonDown("Dash");
     bool isCrouching;
+    bool isDashing;
+    float dashStartTime;
+    float dashTime;
 
     
     [Space][Header("Metrics")]
     public float speed;
     public float jumpForce;
     public float dashForce;
+    public AnimationCurve dashCurve;
     public float dashDuration;
-    float dSpeed = 1f;
+
+
 
 
 
@@ -52,20 +57,32 @@ public class GameController : MonoBehaviour
     {
         DetectInput();
         UpdateCollider();
+
+        if (isDashing)
+        {
+            dashTime = (Time.time - dashStartTime)/dashDuration;
+        }
         
         
     }
 
     private void FixedUpdate()
     {
-        if (!started)
-            return;
-        level.transform.Translate(Vector2.left * speed * dSpeed * Time.deltaTime);
+        if (!running)
+        {
+            if (isDashing && buttonCheck)
+                level.transform.Translate(Vector2.left * (speed + dashForce * (isDashing ? dashCurve.Evaluate(dashTime) : 0)) * Time.deltaTime);
+            else return;
+        }
+            
+            
+        else level.transform.Translate(Vector2.left * (speed + dashForce * (isDashing ? dashCurve.Evaluate(dashTime) : 0 )) * Time.deltaTime);
+
     }
 
     void StartLevel()
     {
-        started = true;
+        running = true;
     }
 
     void DetectInput()
@@ -78,9 +95,15 @@ public class GameController : MonoBehaviour
         if (isDash)
         {
             player.rb.velocity = Vector2.zero;
-            player.rb.gravityScale = 0f; 
-            dSpeed = dashForce;
+            player.rb.gravityScale = 0f;
+            if (!isDashing)
+            {
+                dashStartTime = Time.time;
+                isDashing = true;
+            }
+            
             Invoke("CancelDash", dashDuration);
+            
         }
 
         if (isSlide && player.isGrounded)
@@ -111,7 +134,8 @@ public class GameController : MonoBehaviour
 
     void CancelDash()
     {
-        dSpeed = 1f;
         player.rb.gravityScale = 1f;
+        isDashing = false;
+        dashTime = 0f;
     }
 }
